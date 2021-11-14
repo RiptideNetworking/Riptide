23a194bc050914ba214ea1005502567f85bd676c
Tom Weiland <tomdweiland@gmail.com>
2021-11-13 23:06:28 -0800

    Added file that previously wasn't being copied to the Unity package

---
+++ UnityPackage/Runtime/Transports/ICommon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiptideNetworking.Transports
+{
+    /// <summary>Defines methods, properties, and events which every transport's server and client must implement.</summary>
+    public interface ICommon
+    {
+        /// <summary>Whether or not to output informational log messages. Error-related log messages ignore this setting.</summary>
+        bool ShouldOutputInfoLogs { get; set; }
+
+        /// <summary>Initiates handling of currently queued messages.</summary>
+        /// <remarks>This should generally be called from within a regularly executed update loop (like FixedUpdate in Unity). Messages will continue to be received in between calls, but won't be handled fully until this method is executed.</remarks>
+        void Tick();
+    }
+}
