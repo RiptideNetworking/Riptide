6ee3ec31d374ce2d610ddb8a5465fd42d5189a13
Tom Weiland <tomdweiland@gmail.com>
2022-05-28 22:58:03 -0700

    Remove copyright headers from demo files

--- Demos/Unity/PlayerHostedDemo/Assets/Scripts/Player/CameraController.cs
+++ Demos/Unity/PlayerHostedDemo/Assets/Scripts/Player/CameraController.cs
@@ -1,9 +1,4 @@
-﻿
-// This file is provided under The MIT License as part of RiptideNetworking.
-// Copyright (c) 2022 Tom Weiland
-// For additional information please see the included LICENSE.md file or view it on GitHub: https://github.com/tom-weiland/RiptideNetworking/blob/main/LICENSE.md
-
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace RiptideDemos.RudpTransport.Unity.PlayerHosted
 {
