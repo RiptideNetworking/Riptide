b2a556a5bc4ff6d464d4bc9138d258a7726d6cd6
tom-weiland <51303091+tom-weiland@users.noreply.github.com>
2022-09-22 22:52:41 +0000

    Update site

--- styles/embedmeta.js
+++ styles/embedmeta.js
@@ -1,4 +1,6 @@
 window.onload = function() {
+    var contentAttribute = "content";
+
     var summary = $(".level0.summary > p").text();
     $("meta[property='og:description']").attr(contentAttribute, summary);
     $("meta[property='og:url']").attr(contentAttribute, window.location.href);
