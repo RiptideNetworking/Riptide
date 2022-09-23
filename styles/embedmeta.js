9f628de42891fb7d74225a31c6ce60e987420cc9
tom-weiland <51303091+tom-weiland@users.noreply.github.com>
2022-09-23 01:44:05 +0000

    Update site

--- styles/embedmeta.js
+++ styles/embedmeta.js
@@ -1,10 +1,10 @@
 window.onload = function() {
-    var contentAttribute = "content";
+    // var contentAttribute = "content";
 
-    var summary = $(".level0.summary > p").text();
-    $("meta[property='og:description']").attr(contentAttribute, summary);
-    $("meta[property='og:url']").attr(contentAttribute, window.location.href);
+    // var summary = $(".level0.summary > p").text();
+    // $("meta[property='og:description']").attr(contentAttribute, summary);
+    // $("meta[property='og:url']").attr(contentAttribute, window.location.href);
     
-    var imageProperty = $("meta[property='og:image']");
-    imageProperty.attr(contentAttribute, window.location.origin + "/" + imageProperty.attr(contentAttribute))
+    // var imageProperty = $("meta[property='og:image']");
+    // imageProperty.attr(contentAttribute, window.location.origin + "/" + imageProperty.attr(contentAttribute))
 }
