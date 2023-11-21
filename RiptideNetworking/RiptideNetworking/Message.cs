150efb920835dd5484b8d9b9af6fc1ac391dd194
Tom Weiland <tomdweiland@gmail.com>
2023-11-20 23:23:24 -0800

    Aggressively inline certain overload methods

--- RiptideNetworking/RiptideNetworking/Message.cs
+++ RiptideNetworking/RiptideNetworking/Message.cs
@@ -1691,82 +1691,108 @@ namespace Riptide
         #region Overload Versions
         /// <inheritdoc cref="AddByte(byte)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddByte(byte)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(byte value) => AddByte(value);
         /// <inheritdoc cref="AddSByte(sbyte)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddSByte(sbyte)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(sbyte value) => AddSByte(value);
         /// <inheritdoc cref="AddBool(bool)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddBool(bool)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(bool value) => AddBool(value);
         /// <inheritdoc cref="AddShort(short)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddShort(short)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(short value) => AddShort(value);
         /// <inheritdoc cref="AddUShort(ushort)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddUShort(ushort)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(ushort value) => AddUShort(value);
         /// <inheritdoc cref="AddInt(int)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddInt(int)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(int value) => AddInt(value);
         /// <inheritdoc cref="AddUInt(uint)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddUInt(uint)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(uint value) => AddUInt(value);
         /// <inheritdoc cref="AddLong(long)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddLong(long)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(long value) => AddLong(value);
         /// <inheritdoc cref="AddULong(ulong)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddULong(ulong)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(ulong value) => AddULong(value);
         /// <inheritdoc cref="AddFloat(float)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddFloat(float)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(float value) => AddFloat(value);
         /// <inheritdoc cref="AddDouble(double)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddDouble(double)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(double value) => AddDouble(value);
         /// <inheritdoc cref="AddString(string)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddString(string)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(string value) => AddString(value);
         /// <inheritdoc cref="AddSerializable{T}(T)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddSerializable{T}(T)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add<T>(T value) where T : IMessageSerializable => AddSerializable(value);
 
         /// <inheritdoc cref="AddBytes(byte[], bool)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddBytes(byte[], bool)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(byte[] array, bool includeLength = true) => AddBytes(array, includeLength);
         /// <inheritdoc cref="AddSBytes(sbyte[], bool)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddSBytes(sbyte[], bool)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(sbyte[] array, bool includeLength = true) => AddSBytes(array, includeLength);
         /// <inheritdoc cref="AddBools(bool[], bool)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddBools(bool[], bool)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(bool[] array, bool includeLength = true) => AddBools(array, includeLength);
         /// <inheritdoc cref="AddShorts(short[], bool)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddShorts(short[], bool)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(short[] array, bool includeLength = true) => AddShorts(array, includeLength);
         /// <inheritdoc cref="AddUShorts(ushort[], bool)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddUShorts(ushort[], bool)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(ushort[] array, bool includeLength = true) => AddUShorts(array, includeLength);
         /// <inheritdoc cref="AddInts(int[], bool)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddInts(int[], bool)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(int[] array, bool includeLength = true) => AddInts(array, includeLength);
         /// <inheritdoc cref="AddUInts(uint[], bool)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddUInts(uint[], bool)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(uint[] array, bool includeLength = true) => AddUInts(array, includeLength);
         /// <inheritdoc cref="AddLongs(long[], bool)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddLongs(long[], bool)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(long[] array, bool includeLength = true) => AddLongs(array, includeLength);
         /// <inheritdoc cref="AddULongs(ulong[], bool)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddULongs(ulong[], bool)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(ulong[] array, bool includeLength = true) => AddULongs(array, includeLength);
         /// <inheritdoc cref="AddFloats(float[], bool)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddFloats(float[], bool)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(float[] array, bool includeLength = true) => AddFloats(array, includeLength);
         /// <inheritdoc cref="AddDoubles(double[], bool)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddDoubles(double[], bool)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(double[] array, bool includeLength = true) => AddDoubles(array, includeLength);
         /// <inheritdoc cref="AddStrings(string[], bool)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddStrings(string[], bool)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add(string[] array, bool includeLength = true) => AddStrings(array, includeLength);
         /// <inheritdoc cref="AddSerializables{T}(T[], bool)"/>
         /// <remarks>This method is simply an alternative way of calling <see cref="AddSerializables{T}(T[], bool)"/>.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Message Add<T>(T[] array, bool includeLength = true) where T : IMessageSerializable, new() => AddSerializables(array, includeLength);
         #endregion
         #endregion
