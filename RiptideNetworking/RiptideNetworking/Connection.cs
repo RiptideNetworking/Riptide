605458cf233fa54ae1edf57f5f27ac576017dc06
Per6 <per.daude@t-online.de>
2024-10-12 17:42:05 +0200

    forgot to resend on ack

--- RiptideNetworking/RiptideNetworking/Connection.cs
+++ RiptideNetworking/RiptideNetworking/Connection.cs
@@ -416,6 +416,7 @@ namespace Riptide
 			}
 			while((messageQueue.Count > 0) && (messageQueue[0] == null)) {
 				messageQueue.RemoveFirst();
+				SendData(messageQueue[MaxSynchronousQueuedMessages - 1]);
 			}
 		}
 
